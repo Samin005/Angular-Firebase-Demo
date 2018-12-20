2c543bd51b60bd477f1dc48e98a2a7909e63cd53
Samin005 <samin005.bracu@gmail.com>
2018-12-20 21:13:56 +0600

    Sending and receiving data using angular and firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import {AngularFireDatabase, AngularFireList} from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular-Firebase-Demo';
+  @ViewChild('inputItemName') inputItemName: ElementRef;
+  @ViewChild('inputItemID') inputItemID: ElementRef;
+  @ViewChild('inputItemPrice') inputItemPrice: ElementRef;
+  @ViewChild('inputDeleteItemName') inputDeleteItemName: ElementRef;
+  itemsCouldDB: Observable<any[]>;
+  itemsFirebaseDB: Observable<any[]>;
+  firebaseList: AngularFireList<any>;
+  sortBtnText = 'Price';
+
+  constructor(private CloudDB: AngularFirestore, private firebaseDB: AngularFireDatabase) {
+    this.itemsCouldDB = CloudDB.collection('users').valueChanges();
+    this.itemsFirebaseDB = firebaseDB.list('TestDB/Items').valueChanges();
+    this.firebaseList = firebaseDB.list('TestDB/Items');
+  }
+  addItem() {
+    // console.log(this.inputItemName.nativeElement.value, this.inputItemID.nativeElement.valueAsNumber, this.inputItemPrice.nativeElement.valueAsNumber);
+    this.firebaseList.set(this.inputItemName.nativeElement.value, {name: this.inputItemName.nativeElement.value, id: this.inputItemID.nativeElement.valueAsNumber, price: this.inputItemPrice.nativeElement.valueAsNumber});
+  }
+  deleteItem() {
+    this.firebaseList.remove(this.inputDeleteItemName.nativeElement.value);
+  }
+  sort() {
+    if (this.sortBtnText === 'Price') {
+      this.sortBtnText = 'ID';
+      this.itemsFirebaseDB = this.firebaseDB.list('TestDB/Items', ref => ref.orderByChild('price')).valueChanges();
+    } else {
+      this.sortBtnText = 'Price';
+      this.itemsFirebaseDB = this.firebaseDB.list('TestDB/Items', ref => ref.orderByChild('ID')).valueChanges();
+    }
+  }
 }
